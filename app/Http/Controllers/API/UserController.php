93c4c2326859ffcdbbf43428f39623057419c983
Hujjat Nazari <Hujjat@users.noreply.github.com>
2018-08-26 15:15:17 +1000

    validation for user added

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -29,6 +29,11 @@ class UserController extends Controller
     public function store(Request $request)
     {
 
+        $this->validate($request,[
+            'name' => 'required|string|max:191',
+            'email' => 'required|string|email|max:191|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
 
         return User::create([
             'name' => $request['name'],
