df1a2587005286973ebcece439582cf67160af45
Hujjat Nazari <Hujjat@users.noreply.github.com>
2018-08-28 21:02:02 +1000

    filters added to created_at and user type

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
 import { Form, HasError, AlertError } from 'vform';
 
 window.Form = Form;
@@ -27,6 +28,16 @@ const router = new VueRouter({
     mode: 'history',
     routes // short for `routes: routes`
   })
+
+
+
+Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1)
+});
+
+Vue.filter('myDate',function(created){
+    return moment(created).format('MMMM Do YYYY');
+});
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/assets/js/components/Users.vue
+++ resources/assets/js/components/Users.vue
@@ -28,8 +28,8 @@
                     <td>{{user.id}}</td>
                     <td>{{user.name}}</td>
                     <td>{{user.email}}</td>
-                    <td>{{user.type}}</td>
-                    <td>{{user.created_at}}</td>
+                    <td>{{user.type | upText}}</td>
+                    <td>{{user.created_at | myDate}}</td>
 
                     <td>
                         <a href="#">
