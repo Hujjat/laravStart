e17a92a6ee31578372c48ece8c34e0eae2618273
Hujjat Nazari <Hujjat@users.noreply.github.com>
2018-09-08 17:34:22 +1000

    JWT with Laravel Passport added

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,17 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     /**
      * Display a listing of the resource.
      *
