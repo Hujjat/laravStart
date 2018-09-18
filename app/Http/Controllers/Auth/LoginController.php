cbc4d9e754cae72d4a17424f3fcabd068864da5d
Hujjat Nazari <Hujjat@users.noreply.github.com>
2018-09-18 22:24:01 +1000

    Ready for 5.7

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/overview';
 
     /**
      * Create a new controller instance.
