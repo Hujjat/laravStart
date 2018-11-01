5f4ecb915ff95d2f9de7416be83676e56287a431
Hujjat Nazari <Hujjat@users.noreply.github.com>
2018-11-01 19:03:24 +1100

    invoice page added for printing

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('invoice', function(){
+    return view('invoice');
+});
 
 Route::get('{path}',"HomeController@index")->where( 'path', '([A-z\d-\/_.]+)?' );
